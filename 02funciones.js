76ba493c2d080904e110f3e5d742b3b34411548a
gilpgdmax <gilpgdamx@gmail.com>
2021-12-09 14:55:12 -0600

    Agrega esquema de función

--- 02funciones.js
+++ 02funciones.js
@@ -7,12 +7,10 @@ saluda() // invoca a saluda.
 /* Al terminar la segunda
  * invocación continúa aquí. */
 console.log("adios")
-/* Declaración de function saluda.
- * Primero va la palabra function.
- * Sigue el nombre.
- * Siguen los paréntesis.
- * Finalmente, entre llaves ({})
- * van las instrucciones. */
+
+/**
+ * Definición de la función saluda.
+ */
 function saluda() {
  console.log(nombre)
 }
--- 05funcionesParametrosExpresiones.js
+++ 05funcionesParametrosExpresiones.js
@@ -4,8 +4,8 @@
  * @param {number} c
  */
 function fn(a, b, c) {
- console.log(a + b * c, "")
+ console.log(a + b * c)
 }
 const a = 3
 fn(3 - 8, a, a * 2)
-document.write("ggg")
+console.log("ggg")
